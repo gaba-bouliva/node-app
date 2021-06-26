b251b5567afd0a9293ade96484e5c7e516c420bb
gaba <eaglebald111@gmail.com>
2021-06-26 08:02:43 +0100

    Test Script all good!

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send(
-    'GRATULATION! GABA You have Configured Your CI/CD With Jenkins SUCCESSFULLY!!!'
-  );
+  res.send('GRATULATION! GABA WITH YOUR CI/CD SETUP');
 });
 
 app.listen(process.env.PORT || 4500);
--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var app = require('../index.js');
 describe('GET /', function () {
   it('respond with hello world', function (done) {
     //navigate to root and check the the response is "hello world"
-    request(app).get('/').expect('hello world', done);
+    request(app)
+      .get('/')
+      .expect('GRATULATION! GABA WITH YOUR CI/CD SETUP', done);
   });
 });
